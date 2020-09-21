414290dda8506be58af86979d6eafc17cdfaaadd
Liubomyr <43443223+LiubomyrChereshchuk@users.noreply.github.com>
2020-09-21 22:33:41 +0300

    SortPopup component + router
    add new component + add react router dom

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
+import { SortPopup } from './SortPopup';
 import { Header } from './Header';
 import { Button } from './Button';
 import { Categories } from './Categories';
 
 
-export { Header, Button, Categories };
+
+export { Header, Button, Categories, SortPopup };
